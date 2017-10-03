0cce15824edeaaa6ecfde2c4df83c211e96aaf10
chessesio <flavianchessesio@gmail.com>
2017-10-03 11:11:27 +0000

    Import error resolved

---
+++ Pinterest.py
@@ -0,0 +1,9 @@
+from User import User
+from Pin import Pin
+from Board import Board
+from Signin import Signin
+from Signout import Signout
+from Signup import Signup
+
+def pinterest(option1):
+	pass
