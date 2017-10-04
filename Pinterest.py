02ba2598539e56e49f4e793682ef8f02241181e6
kaytyron <kelvin.tyron@gmail.com>
2017-10-04 19:24:13 +0000

    Pushing the Pinterest module

---
+++ Pinterest.py
@@ -0,0 +1,118 @@
+from User import User
+from Pin import Pin
+from Board import Board
+#from Signup import Signup
+from datetime import datetime
+import os.path
+import csv
+
+
+
+class Pinterest:
+
+    def __init__(self):
+        self.user_list = []
+
+    @staticmethod
+    def signup():
+
+        user = User()
+        new_name = input("Name: ")
+        new_username = input("Username: ")
+        new_email = input("Email: ")
+        new_password = input("Password: ")
+        user.set_name(new_name)
+        user.set_username(new_username)
+        user.set_email(new_email)
+        user.set_password(new_password)
+        user_list.append(user)
+
+        file_exist = os.path.isfile('user.csv')
+        with open('user.csv', 'a') as csv_file:
+            fieldnames = ['username','password']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            if not file_exist:
+                writer.writeheader()
+            writer.writerow({'username': user.get_name(), 'password': user.get_password()})
+
+
+    @staticmethod
+    def signin():
+        """
+        this asks for two parameters to check users in the csv file
+
+        """
+        ask_name = input("Your username? ")
+        ask_password = input("Password: ")
+
+        with open("user.csv", 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            # this checks and loops thru the reader dictionary
+            user_match = False
+            for read in reader:
+                if ask_name == read['username'] and ask_password == read['password']:
+                    user_match = True
+
+            # this prints the output if the user is found
+            if user_match:
+                print('Welcome ', ask_name.capitalize())
+
+
+            else:
+                print('Sorry your password and username don\'t match')
+
+        # this is to add activities to create pins or board
+
+        # activities = int(input("Create pin or board? \n1 - Pin \n 2 - Board"))
+
+        # if activities == 1:
+        #     pin_description = input("Pin description: ")
+        #     pin_title = input("Pin Title: ")
+
+        #     new_user = User()
+            
+        # elif activities == 2:
+        #     # user create boards
+        #     pass
+        # else:
+        #     print("Have a good day!")
+
+
+    def homepage(self):
+    	for user in USER_LIST:
+    		for board in user.boards:
+    			return board.pins
+    
+    def call(self):
+
+        run = True
+        while run == True:
+
+            choice = input("What do you want to do?\n1 Sign in\n2 Sign up\n3 Exit")
+
+            if choice == "1":
+                Pinterest.signin()
+                activities = int(input("Create pin or board? \n1 - Pin \n2 - Board"))
+
+                if activities == 1:
+                    new_user = User()
+                    new_user.create_pin()
+                
+                elif activities == 2:
+                    # user create boards
+                    pass
+                else:
+                    print("Have a good day!")
+
+
+            if choice == "2":
+                Pinterest.signup()
+
+            if choice == "3":
+                print("Goodbye!")
+                run = False
+
+start = Pinterest()
+start.call()
